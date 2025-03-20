716e5f7e73518738c633b73d296ead6492d86549
Maksym Trineiev <mtrineyev@gmail.com>
2025-03-20 16:02:41 +0200

    Constants for subfolders has been replaced to using os.path.join

--- create_folder_structure.py
+++ create_folder_structure.py
@@ -1,20 +1,20 @@
 import os
 import sys
 
-create_fols = [
+FOLDERS = [
     "results",
     "reports",
-    "source_data/instances",
-    "source_data/holdings",
-    "source_data/items",
-    "source_data/users",
-    "source_data/loans",
-    "source_data/courses",
+    os.path.join("source_data", "instances"),
+    os.path.join("source_data", "holdings"),
+    os.path.join("source_data", "items"),
+    os.path.join("source_data", "users"),
+    os.path.join("source_data", "loans"),
+    os.path.join("source_data", "courses"),
 ]
 
 
-def create_fol(iteration):
-    for fol in create_fols:
+def create_fol(iteration: str) -> None:
+    for fol in FOLDERS:
         new_fol = os.path.join("iterations", iteration, fol)
         if not os.path.exists(new_fol):
             os.makedirs(new_fol)
